6bf9fd1a5f71be7f29a83fb686928d2409ad8bb0
SakshiGupta77 <sakshi.gupta_cs17@gla.ac.in>
2021-08-23 13:16:21 +0530

    delegate commit

---
+++ Day8T/AdvDelegate/Study.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Day8T.AdvDelegate
+{
+    public class Study
+    {
+        public string Name(string N)
+        {
+            return N;
+        }
+         public void PhoneNo()
+        {
+            Console.WriteLine("9876");
+        }
+         public bool StudyStatus()
+        {
+            return true;
+        }
+    }
+}
---
+++ Day8T/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Day8T.AdvDelegate;
+//delegate string NameDelegate(string N);
+delegate void PhoneDelegate();
+delegate bool StudyDelegate();
+
+namespace Day8T
+{
+    class Program
+    {
+        static void Main()
+        {
+            Study sObj = new Study();
+            //NameDelegate nameDelegate = new NameDelegate(sObj.Name);
+            //var n = nameDelegate("Sakshi");
+            //Console.WriteLine(n);
+            Func<string,string> func = sObj.Name;
+            string fob = func("sakshi");
+            Console.WriteLine(fob);
+
+        }
+    }
+}
