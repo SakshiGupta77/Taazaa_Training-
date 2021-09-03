c5e423eec8170e1fefedd89fda3d5ea2902de532
SakshiGupta77 <sakshi.gupta_cs17@gla.ac.in>
2021-09-03 11:49:00 +0530

    commit

---
+++ TPLprogram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace TPLprogram
+{
+    class Myapp
+    {
+        public static void LongTask()
+        {
+            Thread.Sleep(6000);
+            Console.WriteLine("going to karolbhag for major, hopfully it will take time");
+        }
+
+        public static void Main() 
+        {
+            Thread obj = new Thread(new ThreadStart(LongTask));
+            obj.Start();
+            Console.WriteLine("i am inside the main function");  
+        }
+    }
+}
